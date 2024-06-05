055fbae1d3fd31416d83cfac42f48de46f81e630
Thabor Walbeek <37037843+DaThabor@users.noreply.github.com>
2024-06-05 11:50:05 +0530

    Add files via upload

--- run-python.py
+++ run-python.py
@@ -1,5 +1,3 @@
-import pandas as pd
+!git clone https://github.com/DaThabor/PublicFiles
 
-url = 'https://raw.githubusercontent.com/DaThabor/Data-Training/master/Source%20Files/WideWorldImports%20csv/Application_Cities.csv?token=GHSAT0AAAAAACSUBNVBALES6MJM5MHRLF72ZS77FCQ'
-df = pd.read_csv(url)
-print(df.head(5))
+%cd PublicFiles
